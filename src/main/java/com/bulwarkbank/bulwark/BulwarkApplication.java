d9172a50fcee127787b1c8e6ffd727178b530cfd
dmgimhana <dm.gimhana@gmail.com>
2024-08-13 19:19:46 +0100

    Project setup

---
+++ src/main/java/com/bulwarkbank/bulwark/BulwarkApplication.java
@@ -0,0 +1,13 @@
+package com.bulwarkbank.bulwark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulwarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulwarkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bulwarkbank/bulwark/BulwarkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bulwarkbank.bulwark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulwarkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
